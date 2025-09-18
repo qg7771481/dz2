13c1f609d3eb56c81d018457c903deb13e8bdf1b
qg7771481 <ddavidok845@gmail.com>
2025-09-18 17:33:16 +0300

    Add files via upload

---
+++ Django2/shop/products/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class TorontoTokyo(models.Model):
+    name = models.CharField(max_length=111)
+    description = models.TextField(blank=False)
+    how_many_games_on_beastmaster = models.IntegerField(max_length=300)
+    city_Toronto = models.IntegerField(max_length=322)
+    prize = models.IntegerField(max_length=30)
+    city_Tokyo = models.CharField(max_length=5)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+
+    def __str__(self):
+        return self.name
